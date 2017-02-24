7470c1906bfbccec9668cf330b3f4a6efe249657
Anthony Garingan <anthonygaringan@Anthony-Garigan.local>
2017-02-23 20:14:30 -1000

    done finished pau

--- js/app.js
+++ js/app.js
@@ -23,12 +23,12 @@ The function will hide the contents in the pargraph with the id of `less` after 
 
 noMore.addEventListener("click", lessContent);
 function lessContent(){
-	document.getElementById("less").style.visibility ="hidden";
+	document.getElementById("less").style.visibility = "hidden";
 	noMore.appendChild(less);
 
 }
 
-
+// another way: less.style.display = "none";
 	
 
 //3. Tacocat, The Original Palindrome King
@@ -46,12 +46,13 @@ function zoom(){
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
 var menuItems = [" Big Mac", " Large Fries",  " Filet-O-Fish"];
-showMenu.addEventListener("click", valueMenu);
+
+//showMenu.addEventListener("click", valueMenu);
 function valueMenu(){
 	showMenu.innerHTML = menuItems;
-	menuItems.appendChild(valueMenu);
-
+	/*menu.innerHTML = menuItems;*/
 }
+
 
 
 //5. Gin.
@@ -59,9 +60,21 @@ function valueMenu(){
 
 
 
+function redFace(){
+
+	drink.style.fontSize = "30px";
+	drink.style.color = "red";
+}
+
+
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+//oreo.addEventListener("onmouseover", showPrice);
+	function showPrice(){
+		price.innerHTML = "$5.55";
+		//price.appendChild(showPrice);
+}
 
 
 //7. Mr. Buttons
@@ -69,7 +82,13 @@ function valueMenu(){
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
+document.getElementById("Benjamin").addEventListener("click", function(){
+	document.getElementById("displayQuote").innerHTML = myQuote;
 
+});
+
+
+	
 
 //8. Say It again, Randomly
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
@@ -77,8 +96,30 @@ var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
 
+random.addEventListener("click", randoQuote);
 
+function randoQuote(){
+		var ran = Math.floor(Math.random()*quotes.length);
+		displayQuotes.innerHTML = quotes[ran];
+	}
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clickig on the button.
 */
+
+document.getElementById("showHide").addEventListener("click", showSecret);
+
+function showSecret(){
+	var secret = document.getElementById("showmoney");
+	if(secret.style.display === "none"){
+		secret.style.display = "block";
+
+	}else{
+		secret.style.display = "none";
+	}
+
+}
+
+
+
+
